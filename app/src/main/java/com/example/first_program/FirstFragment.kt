012c40e2aa9d69e6863dab0af1a193e2bf06a855
gubbih <tlha37727@edu.ucl.dk>
2023-02-15 10:14:15 +0100

    5 out of 7

--- app/src/main/java/com/example/first_program/FirstFragment.kt
+++ app/src/main/java/com/example/first_program/FirstFragment.kt
@@ -32,7 +32,7 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
+        binding.toastButton.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
     }
