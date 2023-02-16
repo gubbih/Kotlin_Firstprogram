10e570d0f557fce32456ca801883256aae2a3e1f
Thor Lund Hansen <tlha37727@edu.ucl.dk>
2023-02-16 14:12:18 +0100

    pushy

--- app/src/main/java/com/example/first_program/FirstFragment.kt
+++ app/src/main/java/com/example/first_program/FirstFragment.kt
@@ -5,8 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import androidx.navigation.fragment.findNavController
 import com.example.first_program.databinding.FragmentFirstBinding
+import com.google.android.material.snackbar.Snackbar
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -28,13 +31,33 @@ class FirstFragment : Fragment() {
         return binding.root
 
     }
-
+    private fun countMe(view: View) {
+        val showCountTextView = view.findViewById<TextView>(R.id.textview_first)
+        val countString = showCountTextView.text.toString()
+        var count = countString.toInt()
+        count++
+        showCountTextView.text = count.toString()
+    }
+    private fun randomVal(view: View) {
+        val showCountTextView = view.findViewById<TextView>(R.id.textview_first)
+        val countString = showCountTextView.text.toString()
+        var count = countString.toInt()
+        count++
+        showCountTextView.text = count.toString()
+    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        binding.toastButton.setOnClickListener {
+        view.findViewById<Button>(R.id.count_button).setOnClickListener{
+            countMe(view)
+        }
+        view.findViewById<Button>(R.id.random_button).setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
+        binding.toastButton.setOnClickListener { view ->
+            Snackbar.make(view, "Hello Toast!", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+
     }
 
     override fun onDestroyView() {
